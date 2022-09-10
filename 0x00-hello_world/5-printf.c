32285df95bb71b08cd8344faa5ca2d273fed2d98
Mbuthia <mbuthiaernest@gmail.com>
2022-09-10 08:11:21 -0700

    Task 5and6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
-
-/**
- *
- * main - Prints text 2 in C
- *
- * Return: Always 0 -(Executable)
- */
-int main(void)
-{	
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}
+ #include <stdio.h>
+ /**
+  * Description: main - Prints out string to standard output.
+  * Return 0 if success.
+  */
+ int main(void)
+ {
+	 printf("with proper grammar, but the outcome is a piece of art,n");
+	 return (0);
+ }
