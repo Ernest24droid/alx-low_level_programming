d06972096409755ef922ff3af268afe7b3b32ce1
Mbuthia <mbuthiaernest@gmail.com>
2022-09-10 08:23:41 -0700

    Final

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
  #include <stdio.h>
-<<<<<<< HEAD
  /**
   * Description: main - Prints out string to standard output.
   * Return 0 if success.
@@ -9,20 +8,3 @@
 	 printf("with proper grammar, but the outcome is a piece of art,n");
 	 return (0);
  }
-=======
-
-/**
- *
- * main - Prints scripts text in th put function
- *
- * Description: using the main function
- * this program prints "with proper grammar, but the outcome is a piece of art"
- * Return: 0
- */
- int main(void)
- {	
-	printf( "with proper grammar, but the outcome is a piece of art, \n" ) ;
-          	return (0) ;
- }  
-
->>>>>>> b9a4f4cf463a035e9666e00377f78d8742147ea3
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  #include <stdio.h>
 
  /**
@@ -19,26 +18,4 @@
 	 printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
 	 return (0);
  }
-=======
-  #include <stdio.h>
 
-  /**
-   * main - Entry point
-   *
-   * Description: prints the sizes of various data type
-   *
-   * Return: Always 0 (Successs)
-   * 
-   */
-
-  int main(void)
-  {
-     
-	  printf("size of a char: %d byte(s)\n", (int)sizeof(char));
-          printf("size of an int: %d byte(s)\n", (int)sizeof(int));
-          printf("size of a long int: %d byte(s)\n" (int)sizeof(long int));
-          printf("size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
-          printf("size of a float: %d byte(s)\n", (int)sizeof(float));
-          return (0);
-}
->>>>>>> b9a4f4cf463a035e9666e00377f78d8742147ea3
