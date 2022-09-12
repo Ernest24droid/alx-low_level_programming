876cc46e389aa038278539b3c235183a05e2eb4c
Mbuthia <mbuthiaernest@gmail.com>
2022-09-12 02:44:39 -0700

    ammended 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
-
 /**
- * main - print quote
- *
- * Description: prints Dora's quote
- *
- * Return Always 1 (Executed)
- */
+* main - print quote
+*
+* Return: Always (Executable)
+*/
 int main(void)
-{write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,17 @@
  */
 int main(void)
 {
-	 printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
-	 printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
-	 printf("Size of a lont int: %d byte(s)\n", (int)sizeof(long int));
-	 printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
-	 printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+
+	 printf("Size of a char: %d byte(s)\n", sizeof(a));
+	 printf("Size of an int: %d byte(s)\n", sizeof(b));
+	 printf("Size of a lont int: %d byte(s)\n", sizeof(c));
+	 printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	 printf("Size of a float: %d byte(s)\n", sizeof(e));
 return (0);
 }
 
